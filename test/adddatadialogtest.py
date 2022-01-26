cf46bf1c2ecf799f91f42d6061fecc1aa13b1919
Sandalas98 <1811552@gmail.com>
2022-01-26 21:38:21 +0100

    Dodano init.py

--- test/adddatadialogtest.py
+++ test/adddatadialogtest.py
@@ -7,4 +7,4 @@ class AddDataDialogTest(unittest.TestCase):
     def test_adddata(self):
         app = App()
         add_data_dialog = dialog.adddatadialog.AddDataDialog(app.container, app)
-        assert(add_data_dialog.controller != None)
+        self.assertTrue(add_data_dialog.controller != None)
