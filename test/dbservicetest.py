45967c54b4d5c025f86b933e31ef80c5c27712bc
lsiemanski <l.siemanski@gmail.com>
2022-01-26 22:05:52 +0100

    Tests

--- test/dbservicetest.py
+++ test/dbservicetest.py
@@ -4,17 +4,36 @@ from app import App
 import logic.dbservice
 
 class DataBaseTest(unittest.TestCase):
-    def test_add_user(self):
-        db = logic.dbservice.DataBase()
-        db.insert_user('michal_test', '1234', 'm.bugno@metrological.com', 'Inzynier')
-        self.assertTrue(db.check_login_data('michal_test', '1234'))
-
     def test_get_results(self):
         db = logic.dbservice.DataBase()
+        db.insert_result({
+            "span": 0.1,
+            "section_height": 0.1,
+            "steel_young_modulus": 0.1,
+            "reinforcement_grade": 0.1,
+            "load": 0.1,
+            "section_width": 0.1,
+            "cover": 0.2,
+            "reinforcement_diameter": 0.1,
+            "concrete_tensile_strength": 0.1,
+            "concrete_young_modulus": 0.1
+        }, 0.1, 'michal_test')
         res = db.get_results()
         self.assertTrue(res != None)
 
     def test_get_samples(self):
         db = logic.dbservice.DataBase()
+        db.insert_sample({
+            "span": 0.1,
+            "section_height": 0.1,
+            "steel_young_modulus": 0.1,
+            "reinforcement_grade": 0.1,
+            "load": 0.1,
+            "section_width": 0.1,
+            "cover": 0.2,
+            "reinforcement_diameter": 0.1,
+            "concrete_tensile_strength": 0.1,
+            "concrete_young_modulus": 0.1
+        }, 0.1)
         sam = db.get_samples()
         self.assertTrue(sam != None)
--- test/nnlogictest.py
+++ test/nnlogictest.py
@@ -5,28 +5,34 @@ from numpy import double
 from app import App
 import logic.nnlogic
 from os.path import exists
+import numpy as np
+
+from logic.dbservice import DataBase
 
 
 class NNLogicTest(unittest.TestCase):
     def test_train_model(self):
+        DataBase().insert_sample({
+            "span": 0.1,
+            "section_height": 0.1,
+            "steel_young_modulus": 0.1,
+            "reinforcement_grade": 0.1,
+            "load": 0.1,
+            "section_width": 0.1,
+            "cover": 0.2,
+            "reinforcement_diameter": 0.1,
+            "concrete_tensile_strength": 0.1,
+            "concrete_young_modulus": 0.1
+        }, 0.1)
         nn = logic.nnlogic.NN()
         nn.train_network()
         self.assertTrue(exists('../trained_NN.json'))
 
     def test_predict(self):
         nn = logic.nnlogic.NN()
-        data = {'rozpietosc' : 100,
-         'wysokosc przekroju' : 100, 
-         'Modul Younga stali' : 100.0, 
-         'stopien zbrojenia' : 5, 
-         'Obciazenie' : 21,
-         'szerokosc przekroju' : 43, 
-         'otulina' : 98.1,
-         'srednica zbrojenia' : 77, 
-         'Wytrzymalosc betonu na rozciaganie' : 51.2,
-         'Modul Younga betonu' : 97}
+        data = np.array([[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]])
         predicted = nn.predict(data)
-        self.assertTrue(predicted != None and predicted is double)
+        self.assertTrue(predicted != None)
 
     def test_create_model(self):
         nn = logic.nnlogic.NN()
